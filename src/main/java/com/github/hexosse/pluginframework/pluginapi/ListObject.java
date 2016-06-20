a584c52c98ab386e2f04f1166696b0f7543eb1c4
hexosse <hexosse@gmail.com>
2016-06-20 21:44:20 +0200

    Added ListObject and MapObject

---
+++ src/main/java/com/github/hexosse/pluginframework/pluginapi/ListObject.java
@@ -0,0 +1,108 @@
+package com.github.hexosse.pluginframework.pluginapi;
+
+/*
+ * Copyright 2016 Hexosse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.*;
+
+/**
+ * @author <b>hexosse</b> (<a href="https://github.comp/hexosse">hexosse on GitHub</a>))
+ */
+public class ListObject<E, PluginClass extends Plugin> extends PluginObject<PluginClass>
+{
+	/**
+	 * The list.
+	 */
+	private ArrayList<E> list = null;
+
+	/**
+	 * @param plugin The plugin that this object belong to.
+	 */
+	public ListObject(PluginClass plugin)
+	{
+		super(plugin);
+		newList();
+	}
+
+	/**
+	 * New instance of the list
+	 */
+	public void newList() {
+		list = new ArrayList<E>();
+	}
+
+	/**
+	 * Appends the specified element to the end of this list.
+	 *
+	 * @param element element to be appended to this list
+	 * @return <tt>true</tt> (as specified by {@link Collection#add})
+	 */
+	public boolean add(E element)
+	{
+		if (!list.contains(element))
+			return list.add(element);
+
+		return false;
+	}
+
+	/**
+	 * Removes the first occurrence of the specified element from this list,
+	 * if it is present.  If the list does not contain the element, it is
+	 * unchanged.  More formally, removes the element with the lowest index
+	 * <tt>i</tt> such that
+	 * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>
+	 * (if such an element exists).  Returns <tt>true</tt> if this list
+	 * contained the specified element (or equivalently, if this list
+	 * changed as a result of the call).
+	 *
+	 * @param element element to be removed from this list, if present
+	 * @return <tt>true</tt> if this list contained the specified element
+	 */
+	public boolean remove(E element)
+	{
+		return list.remove(element);
+	}
+
+	/**
+	 * Returns <tt>true</tt> if this list contains the specified element.
+	 * More formally, returns <tt>true</tt> if and only if this list contains
+	 * at least one element <tt>e</tt> such that
+	 * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+	 *
+	 * @param element element whose presence in this list is to be tested
+	 * @return <tt>true</tt> if this list contains the specified element
+	 */
+	public boolean exist(E element)
+	{
+		return list.contains(element);
+	}
+
+	/**
+	 * Returns the number of elements in this list.
+	 *
+	 * @return the number of elements in this list
+	 */
+	public int size()
+	{
+		return list.size();
+	}
+
+
+	public ArrayList<E> getList()
+	{
+		return list;
+	}
+}
---
+++ src/main/java/com/github/hexosse/pluginframework/pluginapi/MapObject.java
@@ -0,0 +1,121 @@
+package com.github.hexosse.pluginframework.pluginapi;
+
+/*
+ * Copyright 2016 Hexosse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author <b>hexosse</b> (<a href="https://github.comp/hexosse">hexosse on GitHub</a>))
+ */
+public class MapObject<K, V, PluginClass extends Plugin> extends PluginObject<PluginClass>
+{
+	/**
+	 * The map.
+	 */
+	private HashMap<K, V> map = null;
+
+	/**
+	 * @param plugin The plugin that this object belong to.
+	 */
+	public MapObject(PluginClass plugin)
+	{
+		super(plugin);
+		newMap();
+	}
+
+	/**
+	 * New instance of the map
+	 */
+	public void newMap() {
+		map = new HashMap<K, V>();
+	}
+
+	/**
+	 * Associates the specified value with the specified key in this map.
+	 * If the map previously contained a mapping for the key, the old
+	 * value is replaced.
+	 *
+	 * @param key key with which the specified value is to be associated
+	 * @param value value to be associated with the specified key
+	 */
+	public boolean add(K key, V value)
+	{
+		if (!map.containsKey(key))
+			return map.put(key, value) == null ? true : false;
+
+		return false;
+	}
+
+	/**
+	 * Removes the mapping for the specified key from this map if present.
+	 *
+	 * @param  key key whose mapping is to be removed from the map
+	 */
+	public boolean remove(K key)
+	{
+		if(map.containsKey(key))
+			return map.remove(key)== null ? false : true;
+
+		return false;
+	}
+
+	/**
+	 * Returns <tt>true</tt> if this map contains a mapping for the
+	 * specified key.
+	 *
+	 * @param   key   The key whose presence in this map is to be tested
+	 * @return <tt>true</tt> if this map contains a mapping for the specified
+	 * key.
+	 */
+	public boolean exist(K key)
+	{
+		return map.containsKey(key);
+	}
+
+	/**
+	 * Returns the value to which the specified key is mapped,
+	 * or {@code null} if this map contains no mapping for the key.
+	 */
+	public V get(K key)
+	{
+		if(exist(key))
+			return map.get(key);
+		return null;
+	}
+
+	/**
+	 * Returns the number of key-value mappings in this map.
+	 *
+	 * @return the number of key-value mappings in this map
+	 */
+	public int size()
+	{
+		return map.size();
+	}
+
+	/**
+	 * Returns a {@link Set} view of the mappings contained in this map.
+	 *
+	 * @return a set view of the mappings contained in this map
+	 */
+	public Set<Map.Entry<K, V>> entrySet()
+	{
+		return map.entrySet();
+	}
+}
